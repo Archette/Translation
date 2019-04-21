66c7d08b9450b8badf13966ddac17b4c90e7c416
Rixafy <rixafy@gmail.com>
2019-04-21 20:51:44 +0200

    Remove extension config, not it's in archette/language, this ext is no longer needed

--- src/TranslationExtension.php
+++ src/TranslationExtension.php
@@ -5,23 +5,7 @@ declare(strict_types=1);
 namespace Archette\Translation;
 
 use Nette\DI\CompilerExtension;
-use Nette\Schema\Expect;
-use Nette\Schema\Schema;
-use Rixafy\Translation\TranslationConfig;
 
 class TranslationExtension extends CompilerExtension
 {
-	public function getConfigSchema(): Schema
-	{
-		return Expect::structure([
-			'defaultLanguage' => Expect::string('en')
-		]);
-	}
-
-	public function loadConfiguration()
-	{
-		$this->getContainerBuilder()->addDefinition($this->prefix('languageFacade'))
-			->setFactory(TranslationConfig::class)
-			->addSetup('setCurrentLanguage', [$this->config->defaultLanguage]);
-	}
 }
